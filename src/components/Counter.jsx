a8d8dade1ff5a8399706acd09d3a9f61c78a199d
Veronica Langella <verolangi00@gmail.com>
2026-07-05 17:58:04 +0200

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function increase() {
+
+        setCount(count + 1);
+    }
+
+    const resetCount = () => {
+        setCount(0);
+    }
+
+    return (
+
+        <div className="d-grid gap-2 d-md-block">
+            <button onClick={increase} className="btn btn-primary" type="button">Increase</button>
+            <p>Conteggio: {count}</p>
+            <button onClick={resetCount} className="btn btn-primary" type="button">Reset</button>
+        </div>
+
+    )
+};
+
+export default Counter
